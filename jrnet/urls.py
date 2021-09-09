a889621503fe1ec6fb02decd995ef78b9ece175f
jroberts-one <38872371+jroberts-one@users.noreply.github.com>
2021-09-09 10:51:35 -0600

    update – user profile, password reset

--- jrnet/urls.py
+++ jrnet/urls.py
@@ -25,8 +25,10 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', user_views.register, name='register'),
     path('profile/', user_views.profile, name='profile'),
-    path('login/', auth_views.LoginView.as_view(template_name='users/login.html', extra_context={'title': 'login'}), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html', extra_context={'title': 'logout'}), name='logout'),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html',
+                                                extra_context={'title': 'login'}), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html',
+                                                  extra_context={'title': 'logout'}), name='logout'),
     path('password-reset/', auth_views.PasswordResetView.as_view(
         template_name='users/password_reset.html'), name='password_reset'),
     path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(
@@ -35,10 +37,12 @@ urlpatterns = [
         template_name='users/password_reset_confirm.html'), name='password_reset_confirm'),
     path('password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(
         template_name='users/password_reset_complete.html'), name='password_reset_complete'),
+
     path('', include('portfolio.urls'), name='portfolio-home'),
     path('whatscooking/', include('whatscooking.urls'), name='whatscooking-home'),
 ]
 
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
--- portfolio/static/portfolio/js/marathon.js
+++ portfolio/static/portfolio/js/marathon.js
@@ -1,4 +1,4 @@
-function calcuateWeeklyDistance () {
+function calcuateWeeklyDistance() {
   console.log('calcuateWeeklyDistance called');
   $("#trainingTable tr:has(span.distance)").each(function() {
     let weeklySum = 0;
@@ -14,7 +14,7 @@ function calcuateWeeklyDistance () {
 calcuateWeeklyDistance();
 
 
-$( "#unitSelect" ).change(function() {
+$("#unitSelect").change(function() {
   console.log('change event called');
 
   let opt = this.value;
@@ -25,18 +25,15 @@ $( "#unitSelect" ).change(function() {
     let result;
     if (opt == 'imperial') {
       // km to mi
-      let kilometers = parseInt( $(this).html() );
+      let kilometers = parseInt($(this).html());
       result = Math.round(kilometers * factor);
 
     } else if (opt == 'metric') {
       // mi to km
-      let miles = parseInt( $(this).html() );
+      let miles = parseInt($(this).html());
       result = Math.round(miles / factor);
     }
     $(this).html(result);
     calcuateWeeklyDistance();
   });
 });
-
-
-
